e26de9a3fab9e989d0c529346db3aeee568f14f2
Mauro Colella <mauro.ludovico.colella@gmail.com>
2020-07-09 20:00:25 +0800

    Fix doc export + implement adapter creator tool

--- utils/commands/adapter-create.js
+++ utils/commands/adapter-create.js
@@ -1,3 +1,14 @@
+const path = require('path')
+const fs = require('fs')
+const glob = require('glob')
+const mkdirp = require('mkdirp')
+const mustache = require('mustache')
+
+function loadConfig(relativePath) {
+  const cwd = process.cwd()
+  return require(path.resolve(cwd, relativePath))
+}
+
 module.exports = {
   command: 'adapter:create',
   aliases: ['c'],
@@ -6,10 +17,75 @@ module.exports = {
     return args
       .positional('adapter-name', {
         description: 'An identifier for the gateway/module',
-        // required: true,
-      });
+        type: 'string',
+        required: true,
+      })
   },
   handler: (argv) => {
+    // retrieve args
     const adapterName = argv._[1]
+    if (adapterName === undefined) {
+      throw new Error('Missing required adapter name')
+    }
+    // retrieve app version, app prefix, package directory
+    const monorepoInfo = loadConfig('./lerna.json')
+
+    // build template params
+    const { version, packages } = monorepoInfo
+    const params = {
+      packageName: `${process.env['LIB_PREFIX']}/${adapterName}`,
+      packageVersion: version,
+    }
+    console.log(params)
+
+    // Retrieve template structure
+    const templatePath = path.relative(process.cwd(), process.env['LIB_TEMPLATE'])
+    const packageRoot = path.resolve(packages[0].replace(/(\*+)$/, ''))
+    const packageDir = path.resolve(packages[0].replace(/(\*+)$/, adapterName))
+
+    const stripTemplateDirFunc = filePath => filePath.replace(process.env['LIB_TEMPLATE'], '')
+    const globPattern = `${templatePath}/**/*`
+    const inputDirs = glob
+      .sync(`${globPattern}/`)
+      .map(stripTemplateDirFunc)
+
+    const inputFiles = glob
+      .sync(globPattern, { nodir: true })
+      .map(stripTemplateDirFunc)
+
+    console.log(inputDirs)
+    console.log(inputFiles)
+
+    // Create destination
+    const made = mkdirp.sync(packageDir)
+
+    // Reproduce directory structure
+    inputDirs.forEach(inputDir => {
+      mkdirp.sync(path.join(packageDir,inputDir))
+    })
+
+    // For each file
+    inputFiles.forEach(inputFile => {
+      const mustacheExp = /\.mustache$/
+      const isMustache = mustacheExp.test(inputFile)
+
+      // Read and transform template
+      if (isMustache) {
+        const template = fs.readFileSync(path.join(templatePath, inputFile), 'utf-8')
+        const output = mustache.render(template.toString(), params)
+        fs.writeFileSync(
+          path.join(packageDir,inputFile.replace(mustacheExp, '')),
+          output,
+        )
+        // Or pass file through
+      } else {
+        fs.copyFileSync(
+          path.join(templatePath, inputFile),
+          path.join(packageDir,inputFile),
+        )
+      }
+    })
+
+    // recursively copy files there, but if they are `.mustache`, process them as template
   }
 }
--- utils/nodepay-cli.js
+++ utils/nodepay-cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+process.env = { ...process.env, ...require('./config').globals }
 
 const _argv = require('yargs')
   .usage('Usage: yarn cli <command> [options]')
   .commandDir('commands')
   .demandCommand()
-  .showHelp()
+  .help()
   .argv
