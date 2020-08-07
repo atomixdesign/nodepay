4ec67b15e7227f405a92731f587a254209bea97f
Mauro Colella <mauro.ludovico.colella@gmail.com>
2020-08-07 15:37:48 +0800

    Export gateway as strategy

--- packages/nodepay-paystream/src/index.ts
+++ packages/nodepay-paystream/src/index.ts
@@ -1,10 +1,10 @@
-export * from './paystream'
+export { Paystream as Strategy } from './paystream'
 
 export {
   PaystreamPaymentFrequency,
   PaystreamAddress as Address,
-  PaystreamConfig as Config,
   PaystreamCharge as Charge,
+  PaystreamConfig as Config,
   PaystreamCreditCard as CreditCard,
   PaystreamCustomer as Customer,
   PaystreamSubscription as Subscription,
