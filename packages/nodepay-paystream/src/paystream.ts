3cf74b6144fe1bd1caad5c23b80ff5f4fb0d1807
Mauro Colella <mauro.ludovico.colella@gmail.com>
2020-10-15 14:54:00 +0800

    Debug build

--- packages/nodepay-paystream/src/paystream.ts
+++ packages/nodepay-paystream/src/paystream.ts
@@ -58,6 +58,9 @@ export class Paystream extends BaseGateway<PaystreamConfig> implements
       customerDetails as PaystreamAddress,
     )
 
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    console.log(require('util').inspect(customerObject, { depth: 6 }))
+
     await validateOrReject(customerObject)
     return this.api.addCustomer(customerObject)
   }
