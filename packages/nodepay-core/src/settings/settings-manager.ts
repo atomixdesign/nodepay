9961483d6f8c4bac0bd41c081d08e8eee67ce118
Mauro Colella <761780+maurocolella@users.noreply.github.com>
2021-02-10 18:05:29 +0800

    Hotfix dry run/settings manager implementation (#46)
    * Decorate settings calling class
    
    * v0.10.1
    
    * v0.10.2
    
    * v0.10.3

--- packages/nodepay-core/src/settings/settings-manager.ts
+++ packages/nodepay-core/src/settings/settings-manager.ts
@@ -1,7 +1,6 @@
 import { Service } from 'typedi'
 
 export type runMode = 'dry' | 'verbose' | 'wet'
-
 @Service('nodepay.settings')
 export class SettingsManager {
   private _runMode: runMode = 'wet'
